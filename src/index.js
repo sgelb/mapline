0cb41bb95015c14a76246fd1cd5018a851a3bc3e
sgelb <stephan.halbritter@haw-hamburg.de>
2017-01-07 16:57:49 +0100

    Add basic input form for track, style, scale and format

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ var mapboxgl = require('mapbox-gl');
 var mapboxToken = require('./mapboxToken.js');
 mapboxgl.accessToken = mapboxToken();
 
+var form = document.getElementById('config');
+var mapStyle = 'mapbox://styles/mapbox/' + form.style.value + '-v9?optimize=true';
+
 //
-// Interactive map
+// Preview map
 //
 
 var map  = new mapboxgl.Map({
   container: 'map',
   center: [0, 0],
   zoom: 0.5,
-  style: 'mapbox://styles/mapbox/outdoors-v9',
+  style: mapStyle,
 });
 
 map.addControl(new mapboxgl.NavigationControl(), 'top-right');
-map.addControl(new mapboxgl.ScaleControl({maxWidth: 100, unit: 'metric',}));
+map.addControl(new mapboxgl.ScaleControl());
