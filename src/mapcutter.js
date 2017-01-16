ca090ab176b4152cd193aee398a0cbe255b036c6
sgelb <stephan.halbritter@haw-hamburg.de>
2017-01-16 17:06:22 +0100

    Refactor mapcutter

--- src/mapcutter.js
+++ src/mapcutter.js
@@ -1,7 +1,8 @@
 "use strict";
+
 const cheapRuler = require("cheap-ruler");
 
-var paperformats = {
+const paperformats = {
   "a3": [ 297, 420 ],
   "a4": [ 210, 297 ],
   "a5": [ 148, 210 ],
@@ -10,21 +11,18 @@ var paperformats = {
   "legal": [ 216, 356 ]
 };
 
-
 function Bbox(lat) {
+
   if (lat === undefined) {
     throw new Error("Missing argument");
   }
 
-  if (!(this instanceof Bbox)) {
-    return new Bbox(lat);
-  }
-
+  this.lat = lat;
   this.ruler = cheapRuler(lat, "meters");
 
   // new bbox [w, s, e, n]
   this.bbox = [ Infinity, Infinity, -Infinity, -Infinity ];
-}
+};
 
 // extent bbox to include coordinate
 Bbox.prototype.include = function(ll) {
@@ -34,49 +32,51 @@ Bbox.prototype.include = function(ll) {
   this.bbox[3] = Math.max(this.bbox[3], ll[1]);
 };
 
+// return dimensions of bbox in meters
+Bbox.prototype.dimensions = function() {
+  // distance between nw and ne corner of bbox
+  // TODO: replace distance() with own squaredDistance() and benchmark
+  var w = this.ruler.distance(
+    [ this.bbox[0], this.bbox[3] ],
+    [ this.bbox[2], this.bbox[3] ]);
+  // distance between nw and sw corner of bbox
+  var h = this.ruler.distance(
+    [ this.bbox[0], this.bbox[3] ],
+    [ this.bbox[0], this.bbox[1] ]);
+  return [w, h];
+};
+
 // return if bbox width and height are larger than arguments
 Bbox.prototype.largerThan = function(maxWidth, maxHeight) {
-  // distance between nw and ne corner of bbox
-  var width = this.ruler.distance([ this.bbox[0], this.bbox[3] ], [
-    this.bbox[2],
-    this.bbox[3]
-  ]);
-  // distance between nw and sw corner of bbox
-  var height = this.ruler.distance([ this.bbox[0], this.bbox[3] ], [
-    this.bbox[0],
-    this.bbox[1]
-  ]);
-
-  return (width > maxWidth || height > maxHeight) && (height > maxWidth || width > maxHeight)
+  let [w, h] = this.dimensions();
+  return (w > maxWidth || h > maxHeight) && (h > maxWidth || w > maxHeight);
 };
 
 // resize bbox to width and height
-Bbox.prototype.resize = function(width, height) {
-  var w = this.ruler.distance(
-    [ this.bbox[0], this.bbox[3] ], 
-    [ this.bbox[2], this.bbox[3] ]);
-  var h = this.ruler.distance(
-    [ this.bbox[0], this.bbox[3] ], 
-    [ this.bbox[0], this.bbox[1] ]);
-
+Bbox.prototype.resize = function (width, height) {
+  var [w, h] = this.dimensions();
   var wDiff = 0.5 * (width - w);
   var hDiff = 0.5 * (height - h);
 
   if (h <= width && w <= height) {
     wDiff = 0.5 * (height - w);
-    hDiff = 0.5 * (width - h);
+    hDiff = 0.5 * (width- h);
   }
 
-  this.bbox[0] = this.ruler.destination([this.bbox[0], this.bbox[3]], wDiff, 270)[0];
-  this.bbox[1] = this.ruler.destination([this.bbox[0], this.bbox[1]], hDiff, 180)[1];
-  this.bbox[2] = this.ruler.destination([this.bbox[2], this.bbox[3]], wDiff, 90)[0];
-  this.bbox[3] = this.ruler.destination([this.bbox[0], this.bbox[3]], hDiff, 0)[1];
+  this.bbox[0] = this.ruler.destination(
+    [this.bbox[0], this.bbox[3]], wDiff, 270)[0];
+  this.bbox[1] = this.ruler.destination(
+    [this.bbox[0], this.bbox[1]], hDiff, 180)[1];
+  this.bbox[2] = this.ruler.destination(
+    [this.bbox[2], this.bbox[3]], wDiff, 90)[0];
+  this.bbox[3] = this.ruler.destination(
+    [this.bbox[0], this.bbox[3]], hDiff, 0)[1];
 };
 
 // revert to bbox
 Bbox.prototype.revertTo = function(bbox) {
   this.bbox = bbox;
-}
+};
 
 // return bbox as LineString array
 Bbox.prototype.linestring = function() {
@@ -89,12 +89,30 @@ Bbox.prototype.linestring = function() {
   ];
 };
 
+// return bbox as Feature of type LineString
+Bbox.prototype.feature = function() {
+  return {"type": "Feature",
+    "geometry": {
+      "type": "LineString",
+      "coordinates": this.linestring()
+    },
+    "properties": {"name": "cutout"
+    }
+  };
+};
+
+
+function featurecollection(features) {
+  return {
+    "type": "FeatureCollection",
+    "features": features
+  };
+}
+
 var mapcutter = {};
 
-mapcutter.bbox = Bbox;
-
 // return array of sheet bboxes in specified scale and format along track
-mapcutter.bboxes = function(track, scale, format, fn) {
+mapcutter.featurecollection = function(track, scale, format) {
   var format = paperformats[format];
 
   // real world width and height of map sheet in meter
@@ -102,7 +120,7 @@ mapcutter.bboxes = function(track, scale, format, fn) {
   var rh = format[1] / 1000 * scale;
 
   // padding around track in mm
-  let padding = 10;
+  var padding = 10;
   var rwp = (format[0] - padding) / 1000 * scale;
   var rhp = (format[1] - padding) / 1000 * scale;
 
@@ -110,29 +128,27 @@ mapcutter.bboxes = function(track, scale, format, fn) {
 
   // calculate real world width and height of map sheet
   for (let feature of track.features) {
-    let bbox = Bbox(feature.geometry.coordinates[0][1]);
+    let bbox = new Bbox(feature.geometry.coordinates[0][1]);
     for (let coord of feature.geometry.coordinates) {
       let oldBbox = bbox.bbox;
       bbox.include(coord);
       if (bbox.largerThan(rwp, rhp)) {
-        // FIXME: look if distance between coords > rwp||rhp?
+        // FIXME: edge case of distance between coords > max(rwp, rhp)?
         bbox.revertTo(oldBbox);
         bbox.resize(rw, rh);
 
-        fn(Math.random().toString(36).substring(2, 5), bbox.linestring());
-        bboxes.push(bbox.bbox);
+        bboxes.push(bbox.feature());
 
-        bbox = Bbox(coord[1]);
+        bbox = new Bbox(coord[1]);
         bbox.include(coord);
       }
     }
-    
+
     bbox.resize(rw, rh);
-    fn(Math.random().toString(36).substring(2, 5), bbox.linestring());
-    bboxes.push(bbox.bbox);
+    bboxes.push(bbox.feature());
   }
 
-  return bboxes;
+  return featurecollection(bboxes);
 };
 
 module.exports = mapcutter;
