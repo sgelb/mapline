0249bd11a850e04c2580717187cf06fd4c06bba7
sgelb <stephan.halbritter@haw-hamburg.de>
2020-12-10 20:35:19 +0100

    Enable CORS for overpass query

--- src/overpass.js
+++ src/overpass.js
@@ -6,59 +6,59 @@ import queryOverpass from "@derhuerst/query-overpass";
 const mapping = new Map();
 mapping.set("atm", {
   query: "amenity~'atm|bank'",
-  sym: "bank-11"
+  sym: "bank-11",
 });
 mapping.set("bakery", {
   query: "shop=bakery",
-  sym: "bakery-11"
+  sym: "bakery-11",
 });
 mapping.set("bike_shop", {
   query: "shop=bicycle",
-  sym: "bicycle-11"
+  sym: "bicycle-11",
 });
 mapping.set("cafe", {
   query: "amenity=cafe",
-  sym: "cafe-11"
+  sym: "cafe-11",
 });
 mapping.set("camping", {
   query: "tourism=camp_site",
-  sym: "campsite-11"
+  sym: "campsite-11",
 });
 mapping.set("drinking_water", {
   query: "amenity=drinking_water",
-  sym: "drinking-water-11"
+  sym: "drinking-water-11",
 });
 mapping.set("graveyard", {
   query: "landuse=cemetery][amenity=grave_yard",
-  sym: "cemetery-11"
+  sym: "cemetery-11",
 });
 mapping.set("hospitals", {
   query: "amenity=hospital",
-  sym: "hospital-11"
+  sym: "hospital-11",
 });
 mapping.set("information", {
   query: "information=office",
-  sym: "information-11"
+  sym: "information-11",
 });
 mapping.set("pharmacy", {
   query: "amenity=pharmacy",
-  sym: "pharmacy-11"
+  sym: "pharmacy-11",
 });
 mapping.set("picnic", {
   query: "tourism=picnic_site",
-  sym: "picnic-site-11"
+  sym: "picnic-site-11",
 });
 mapping.set("restaurant", {
   query: "amenity=restaurant",
-  sym: "restaurant-11"
+  sym: "restaurant-11",
 });
 mapping.set("shelter", {
   query: "amenity=shelter",
-  sym: "shelter-11"
+  sym: "shelter-11",
 });
 mapping.set("supermarket", {
   query: "shop~'supermarket|convenience'",
-  sym: "grocery-11"
+  sym: "grocery-11",
 });
 
 function queryPOI(bbox, tag) {
@@ -66,21 +66,21 @@ function queryPOI(bbox, tag) {
     throw new Error("Tag " + tag + " unknown");
   }
 
-  return queryOverpass(query(bbox, tag))
-    .then(elements => {
+  return queryOverpass(query(bbox, tag), { fetchmode: "cors" })
+    .then((elements) => {
       let pois = [];
-      elements.forEach(ele =>
+      elements.forEach((ele) =>
         pois.push({
           coords: [ele.lon, ele.lat],
           props: {
             name: ele.tags.name || tag[0].toUpperCase() + tag.substr(1),
-            symbol: mapping.get(tag).sym
-          }
+            symbol: mapping.get(tag).sym,
+          },
         })
       );
       return pois;
     })
-    .catch(e => {
+    .catch((e) => {
       throw e;
     });
 }
@@ -90,7 +90,7 @@ function query(bbox, tag) {
     bbox.getSouth(),
     bbox.getWest(),
     bbox.getNorth(),
-    bbox.getEast()
+    bbox.getEast(),
   ].join(",");
 
   return `\
@@ -102,24 +102,24 @@ function query(bbox, tag) {
 const overpass = {
   loadPOIs(features, tag) {
     const promises = [];
-    features.forEach(feature => {
+    features.forEach((feature) => {
       promises.push(queryPOI(feature.bbox, tag));
     });
 
     return Promise.all(promises)
-      .then(allElements => {
+      .then((allElements) => {
         return allElements.reduce((prev, cur) => {
           return prev.concat(cur);
         });
       })
-      .catch(e => {
+      .catch((e) => {
         throw e;
       });
   },
 
   mapping() {
     return mapping;
-  }
+  },
 };
 
 export default overpass;
